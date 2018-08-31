3c60bd5cd063a1af4d52b7859906e93ce86e69f1
Don Davis <dcaa2005@hotmail.com>
2018-08-31 16:50:55 -0600

    original app functionality restored

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,50 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios'
+
 
 Vue.use(Vuex)
 
+const musicApi = Axios.create({
+  baseURL: '//itunes.apple.com/search?term=',
+  timeout: 3000
+})
+
 export default new Vuex.Store({
   state: {
+    songs: []
 
   },
   mutations: {
+    setSongs(state, songs) {
+      let mySongs = []
+      songs.forEach(song => {
+
+        let mySong = {
+          id: song.trackId,
+          title: song.trackName,
+          albumArt: song.artworkUrl60.replace(/60x60/g, "150x150"),
+          artist: song.artistName,
+          collection: song.collectionName,
+          price: song.trackPrice,
+          preview: song.previewUrl
+        }
+        mySongs.push(mySong)
+      })
+      state.songs = mySongs
+      console.log("state songs: ", state.songs)
+    }
 
   },
   actions: {
 
+    getMusicByArtist({ commit, dispatch }, artist) {
+      musicApi.get(artist)
+        .then(res => {
+          console.log(res.data.results)
+          commit('setSongs', res.data.results)
+        })
+        .catch(err => console.error(err.message))
+    }
   }
 })
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,167 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row home justify-content-left">
+    <!-- LEFT PANEL -->
+    <div id="left-panel" class="col-md-6 col-xs-12">
+      <div class="row">
+        <div id="control-panel" class="col-md-6 offset-md-6 col-xs-12">
+          <h1 id="welcome" class="mt-2">
+            <strong>Welcome to d2Tunes2</strong>
+          </h1>
+          <p class="text-white">Powered by iTunes
+            <i class="fab fa-itunes"></i>
+          </p>
+          <form class="form-inline" @submit.prevent="getMusic">
+            <!--DO NOT MODIFY THE ID OR ONCLICK ATTRIBUTES IN THIS FORM-->
+            <div class="form-group mt-2 p-2">
+              <input type="text" class="form-control mt-2 mb-2 mr-2" name="artist" placeholder="Artist Name" v-model="artist" />
+              <button type="submit" class="btn btn-primary mt-2 mb-2" id="get-music-button">Get Music</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <!-- RIGHT PANEL -->
+    <div id="right-panel" class="col-md-6 offset-md-6 col-xs-12">
+      <div id="songs" class="row">
+        <div v-for="song in songs" :key="song.id" class="col-md-6 col-xs-12 song-card">
+          <div class="card bg-light mb-3">
+            <h3 class="card-header">{{song.artist}}</h3>
+            <div class="card-body">
+              <h5 class="card-title" @click="playTitle(song.id)">{{song.title}}</h5>
+              <h6 class="card-subtitle text-muted">{{song.collection}}</h6>
+            </div>
+            <img style="width: 100%; display: block;" :src="song.albumArt" alt="Card image">
+            <div class="card-body">
+              <audio controls :id="song.id" @play="prevPlay(song.id)">
+                <source :src="song.preview" type="audio/ogg">
+                <source :src="song.preview" type="audio/aac">
+                <source :src="song.preview" type="audio/mp4"> Your browser does not support the audio element.
+              </audio>
+            </div>
+            <div class="card-footer">
+              Price: {{song.price}} @ itunes.apple.com
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+
+  export default {
+    name: 'home',
+    data() {
+      return {
+        artist: '',
+        activePlayer: undefined
+      }
+    },
+    components: {
+
+    },
+    methods: {
+
+      getMusic() {
+        this.$store.dispatch('getMusicByArtist', this.artist)
+        this.artist = ""
+      },
+
+      //this function will pause the previously selected audio track and set
+      //current track as the active player
+      prevPlay(id) {
+        if (this.activePlayer) {
+          this.activePlayer.pause()
+        }
+        this.activePlayer = document.getElementById(id)
+      },
+
+
+      //this function plays the preview for the selected title element
+      playTitle(id) {
+        let aud = document.getElementById(id)
+        console.log(aud)
+        //@ts-ignore
+        aud.play()
+      }
+    },
+
+    computed: {
+      songs() {
+        return this.$store.state.songs
+      }
+    }
   }
-}
 </script>
+
+<style>
+  #welcome {
+    color: white;
+  }
+
+
+  body {
+    background-color: black;
+    height: 100vh;
+  }
+
+  #left-panel {
+    position: fixed;
+    top: 0;
+    height: 100vh;
+    background-image: url("../assets/music.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+
+  }
+
+  .flexbox {
+    display: flex;
+    justify-content: center
+  }
+
+  .form-group {
+    border: 1px solid #325d88;
+    border-radius: 10px;
+    background-color: #d3d3d0
+  }
+
+  .card-header,
+  .card-footer {
+    color: white;
+    background: #325d88
+  }
+
+  .card-title {
+    cursor: pointer;
+  }
+
+  audio {
+    width: 90%;
+  }
+
+  @media screen and (max-width: 768px) {
+    .song-card {
+      position: relative;
+      z-index: 0;
+      padding-top: 230px;
+    }
+  }
+
+  .card {
+    z-index: auto;
+    position: relative;
+  }
+
+  #control-panel {
+    /* position: fixed;
+    z-index: 10; */
+    text-align: left;
+  }
+
+  .right-panel {
+    min-width: 50%;
+  }
+</style>
